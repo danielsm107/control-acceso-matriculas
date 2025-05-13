74c03591fe624f178c011fbdf4c5f512dd21864c
danielsm107 <danielserranomarin821@gmail.com>
2025-05-13 13:59:08 +0200

    Arreglar las rutas en los html

--- backend/routes/api.py
+++ backend/routes/api.py
@@ -10,7 +10,7 @@ api = Blueprint('api', __name__)
 
 @api.route("/recibir_matricula", methods=["POST"])
 def recibir_matricula():
-    from app import socketio  # evitar import circular si socketio se usa en app.py
+    from app import socketio
 
     if request.content_type.startswith("multipart/form-data"):
         matricula = request.form.get("matricula")
@@ -41,20 +41,13 @@ def recibir_matricula():
         ruta_imagen = os.path.join("static/imagenes", nombre_imagen)
         imagen.save(ruta_imagen)
 
-    fecha_actual = datetime.now(pytz.timezone("Europe/Madrid")).strftime("%Y-%m-%d %H:%M:%S")
+    # Bloque para evitar duplicados consecutivos
+    cursor.execute("""
+        SELECT matricula FROM registros_accesos
+        ORDER BY fecha DESC LIMIT 1
+    """)
+    ultimo_registro = cursor.fetchone()
 
-    cursor.execute(
-        "INSERT INTO registros_accesos (matricula, estado, imagen, fecha) VALUES (%s, %s, %s, %s)",
-        (matricula, estado, nombre_imagen, fecha_actual)
-    )
-    conexion.commit()
+    if ultimo_registro and ultimo_registro[0] == matricula:
+        conexion.close
 
-    socketio.emit("nuevo_acceso", {
-        "matricula": matricula,
-        "estado": estado,
-        "fecha": fecha_actual,
-        "imagen": nombre_imagen
-    })
-
-    conexion.close()
-    return jsonify({"acceso": estado, "imagen": nombre_imagen})
